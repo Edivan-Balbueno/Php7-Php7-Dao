5c1151be155004dcfd66cdd6294a1a8e4cac44b0
Edivan-Balbueno <37645592+Edivan-Balbueno@users.noreply.github.com>
2018-06-19 23:05:27 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
 require_once("config.php");
-
+/*
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
+*/
+
+$root = new Usuario();
+$root->loadById(7);
+
+echo "$root";
 
 ?>
