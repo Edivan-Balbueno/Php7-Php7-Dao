5f2b0bf5fffbc90260240f46b6b51fffced116f1
Edivan-Balbueno <37645592+Edivan-Balbueno@users.noreply.github.com>
2018-06-25 22:14:55 -0400

    Add files via upload

---
+++ classe/Usuario.php
@@ -0,0 +1,84 @@
+aula 63
+pdo dao select
+
+<?php
+
+class Usuario {
+
+	//variavéis
+	private $idusuario;
+	private $deslogin;
+	private $dessenha;
+	private $dtcadastro;
+
+	//gets e sets
+	public function getIdusuario(){
+		return $this->idusuario;
+	}
+	public function setIdusuario($value){
+		$this->idusuario = $value;
+	}
+
+	public function getDeslogin(){
+		return $this->deslogin;
+	}
+	public function setDeslogin($value){
+		$this->deslogin = $value;
+	}
+
+	public function getDessenha(){
+		return $this->dessenha;
+	}
+	public function setDessenha($value){
+		$this->dessenha = $value;
+	}
+
+	public function getDtcadastro(){
+		return $this->dtcadastro;
+	}
+	public function setDtcadastro($value){
+		$this->dtcadastro = $value;
+	}
+
+	//sql
+	public function loadById($id){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",
+			array(":ID"=>$id)
+		);
+
+		//if (isset($results[0])){
+		if (count($results) > 0) {
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}
+	}
+
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
+
+	public function __toString(){
+
+		return json_encode(array(
+			"idusuario"=>$this->getIdusuario(),
+			"deslogin"=>$this->getDeslogin(),
+			"dessenha"=>$this->getDessenha(),
+			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")));
+
+	}
+
+}
+
+?>
