3c5a3b3d2c11263e9f60ed811abf630eba389d52
Edivan-Balbueno <37645592+Edivan-Balbueno@users.noreply.github.com>
2018-07-24 23:12:51 -0300

    Add files via upload

---
+++ classe/Usuario.php
@@ -0,0 +1,137 @@
+<?php
+
+class Usuario {
+
+	//variavéis
+	private $idusuario;
+	private $deslogin;
+	private $dessenha;
+	private $dtcadastro;
+
+	//gets e sets
+	public function getIdusuario(){
+		return $this->idusuario;
+	}
+	public function setIdusuario($value){
+		$this->idusuario = $value;
+	}
+
+	public function getDeslogin(){
+		return $this->deslogin;
+	}
+	public function setDeslogin($value){
+		$this->deslogin = $value;
+	}
+
+	public function getDessenha(){
+		return $this->dessenha;
+	}
+	public function setDessenha($value){
+		$this->dessenha = $value;
+	}
+
+	public function getDtcadastro(){
+    return $this->dtcadastro;
+}
+	public function setDtcadastro($value){
+		$this->dtcadastro = $value;
+	}
+
+	//sql
+	public function loadById($id){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",
+			array(":ID"=>$id)
+		);
+
+		//if (isset($results[0])){
+		if (count($results) > 0) {
+
+			$this->setData($results[0]);
+		}
+	}
+
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%"
+		));
+
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD",
+			array(
+				":LOGIN"=>$login,
+				":PASSWORD"=>$password)
+		);
+
+		//if (isset($results[0])){
+		if (count($results) > 0) {
+
+			$this->setData($results[0]);
+
+		} else {
+
+			throw new Exception("Login e/ou senha inválido.");
+			
+		}
+	}
+
+	public function setData($data){
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+	}
+
+	public function insert(){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+
+		if (count($results) > 0) {
+			$this->setData($results[0]);
+		}
+
+	}
+
+	public function __construct($login = "", $password = "") {
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+
+	}
+
+	public function __toString(){
+
+		return json_encode(array(
+			"idusuario"=>$this->getIdusuario(),
+			"deslogin"=>$this->getDeslogin(),
+			"dessenha"=>$this->getDessenha(),
+			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")));
+
+	}
+
+}
+
+
+
+?>
